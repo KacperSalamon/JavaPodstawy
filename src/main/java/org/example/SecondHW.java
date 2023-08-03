7ed0de8ff0350434b0e89a44646888f6d75619ed
Kacper Salamon <kacper.salamon@comarch.com>
2023-08-03 10:35:20 +0200

    Second + Third HW

--- src/main/java/org/example/SecondHW.java
+++ src/main/java/org/example/SecondHW.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class SecondHW {
     public static void main(String[] args) {
-        System.out.println("Zaczynam naukę na kursie: “Java od Podstaw”");
+        System.out.println("Zaczynam naukę na kursie: \"Java od Podstaw\"");
         System.out.println("Pora więc na podstawy\n");
 
         System.out.println("\tJ");
