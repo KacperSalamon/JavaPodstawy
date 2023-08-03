a057f39aab1ea3197d6d4c10860bd9271cc740f9
Kacper Salamon <kacper.salamon@comarch.com>
2023-08-03 10:33:27 +0200

    Second + Third HW

---
+++ src/main/java/org/example/ThirdHW.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class ThirdHW {
+    public static void main(String[] args) {
+        System.out.println("\"Wyrażenia\" generują nowe dane");
+        System.out.println("\"Zmienne\" przechowują dane.");
+        System.out.println("Operacje \"Input/output\" umożliwia interakcję z użytkownikiem.");
+        System.out.println("\"Instrukcje warunkowe\" sterują przebiegiem programu.");
+    }
+}
