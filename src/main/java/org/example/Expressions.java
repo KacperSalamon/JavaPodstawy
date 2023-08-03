78b88bdc86b39a28cf1605ad02bac6a401aefe88
Kacper Salamon <kacper.salamon@comarch.com>
2023-08-03 11:35:37 +0200

    Second + Third HW

---
+++ src/main/java/org/example/Expressions.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Expressions {
+    public static void main(String[] args) {
+        //4 homework
+
+        System.out.println(4 + 9); //4.1
+        System.out.println("text" + 10);//4.2
+        System.out.println(6.0 / 4);//4.3
+        System.out.println(10 > 5);//4.4
+
+        //5 homework
+
+        System.out.println(5 * 10); //5.1
+        System.out.println((10.0+20.0)/7.0);//5.2
+        //5.3
+        double pow = Math.pow(15, 2);
+        System.out.println(pow > 100);
+        //5.4
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(a + " + " + b + " is " + sum);
+        
+        double c = 30.0;
+        double d = 7.0;
+        double divisionResult = c / d;
+        System.out.println(c + " / " + d + " daje " + divisionResult);
+
+
+    }
+}
