7d9be1c09fa814ede43427931dbde23ae8ecc4af
Kacper Salamon <kacper.salamon@comarch.com>
2023-08-03 10:18:48 +0200

    First Commit - cats

---
+++ src/main/java/org/example/FirstProgram.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Witaj");
+        System.out.println("");
+        System.out.println("Co słychać");
+    }
+}
