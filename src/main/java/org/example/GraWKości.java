a11eb3c5d14544e58e624c390a83a3c0b2fad87a
Kacper Salamon <kacper.salamon@comarch.com>
2023-08-03 16:33:50 +0200

    gra w kości

---
+++ src/main/java/org/example/GraWKości.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GraWKości {
+    static Random random = new Random();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int wynik = 0;
+
+        System.out.println("Gra w kości - Pierwsza wersja");
+        System.out.println("Trzy tury, zdobądź jak najwięcej punktów!");
+
+        while (wynik < 20) {
+            //System.out.println("Tura " + tura);
+            System.out.println("Chcesz rzucić kostką czy spasować?");
+            System.out.println("Twój wynik to: " + wynik);
+            System.out.println("1. Rzucam");
+            System.out.println("2. Pasuję");
+
+            int wybor = scanner.nextInt();
+
+            if (wybor == 1) {
+                int rzut = returnThrow();
+                System.out.println("Rzucasz... wypadło " + rzut);
+                if (rzut == 1) {
+                    wynik = 0;
+                    System.out.println("Niestety wypadła jedynka! Tracisz punkty w tym rzucie.");
+                    break;
+                } else {
+                    wynik += rzut;
+                }
+
+            } else if (wybor == 2) {
+                System.out.println("Pasujesz. Twój wynik w tej turze: " + wynik);
+                break;
+            } else {
+                System.out.println("Nieprawidłowy wybór. Spróbuj ponownie.");
+            }
+        }
+
+        System.out.println("Koniec gry! Tym razem uzbierano " + wynik + " punktów.");
+        scanner.close();
+
+
+    }
+
+    public static int returnThrow() {
+        //Random random = new Random();
+        return random.nextInt(6) + 1;
+    }
+}
---
+++ src/main/java/org/example/GraWKości2.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GraWKości2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int wynik = 0;
+        int tymczasowaPula = 0;
+        int trwalaPula = 0;
+
+        System.out.println("Gra w kości - Wersja 2");
+        System.out.println("Zdobądź 20 punktów, ale uważaj, jeśli wypadnie 1 to przegrywasz!");
+
+        while (trwalaPula < 20) {
+
+            System.out.println("Chcesz rzucić kostką czy spasować?");
+            System.out.println("Twój wynik to: " + tymczasowaPula);
+            System.out.println("1. Rzucam");
+            System.out.println("2. Pasuję");
+
+
+            int wybor = scanner.nextInt();
+
+            if (wybor == 1) {
+                int rzut = random.nextInt(6) + 1;
+                System.out.println("Rzucasz... wypadło " + rzut);
+
+                if (rzut == 1) {
+                    tymczasowaPula = 0;
+                    System.out.println("Niestety wypadła jedynka! Tracisz punkty w tym rzucie.");
+                    break;
+                } else {
+                    tymczasowaPula += rzut;
+                }
+            } else if (wybor == 2) {
+                trwalaPula += tymczasowaPula;
+                tymczasowaPula = 0;
+                System.out.println("Pasujesz. Trwała pula: " + trwalaPula);
+                break;
+            } else {
+                System.out.println("Nieprawidłowy wybór. Spróbuj ponownie.");
+            }
+
+        }
+
+        System.out.println("Koniec gry! Tym razem uzbierano " + trwalaPula + " punktów.");
+        scanner.close();
+    }
+}
